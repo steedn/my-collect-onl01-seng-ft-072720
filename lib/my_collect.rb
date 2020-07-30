3871ab5899f88063fdc15a3f6090db6190d68584
Nathan Steed <steedn@outlook.com>
2020-07-30 23:02:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require "pry"
 
-
+def my_collect(array)
+  new_array = []
+  i = 0
+  while array.length > 0
+    new_array << yield(array[i])
+    i += 1
+    break if i >= array.length
+    # binding.pry
+  end
+  new_array
+end
