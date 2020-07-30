5e2b53de74dcedc151ab882fcb4f09193fda8299
Nathan Steed <steedn@outlook.com>
2020-07-30 23:01:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,10 @@ require "pry"
 def my_collect(array)
   new_array = []
   i = 0
-  while i < array.length
+  while array.length > 0
     new_array << yield(array[i])
     i += 1
+    break if i >= array.length
     # binding.pry
   end
   new_array
